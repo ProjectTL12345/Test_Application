e9002176da70347fa8145419e52498d15cfef85c
Project_TL <kimpiao020@gmail.com>
2021-02-21 00:40:17 +0900

    init

---
+++ Test_CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test_CSharp {
+    class Program {
+        static void Main(string[] args) {
+            string text1 = "Hello";
+            string text2 = ", World! ";
+            string typeMessage = Console.ReadLine();
+            
+            Console.Write(text1 + text2 + typeMessage);
+        }
+    }
+}
