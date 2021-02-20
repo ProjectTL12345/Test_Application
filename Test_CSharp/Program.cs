60a71314a33f46081da34c07f285e636d0e58a13
Project_TL <kimpiao020@gmail.com>
2021-02-21 01:18:20 +0900

    Add: Add function

--- Test_CSharp/Program.cs
+++ Test_CSharp/Program.cs
@@ -5,9 +5,18 @@ namespace Test_CSharp {
         static void Main(string[] args) {
             string text1 = "Hello";
             string text2 = ", World! ";
+
+            Console.WriteLine("Please type another string!");
             string typeMessage = Console.ReadLine();
-            
-            Console.Write(text1 + text2 + typeMessage);
+
+            Console.Write("{0}{1}{2}\n", text1, text2, typeMessage); // This is C Style
+
+            Program program = new Program();
+            Console.WriteLine(program.Plus(10, 10));
+        }
+
+        int Plus(int number1, int number2) { // This is new function
+            return number1 + number2;
         }
     }
 }
